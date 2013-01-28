b71523302544b70ad9238c790053c9d07fd125ec
Victor Neo <icyisamu@gmail.com>
2013-01-28 16:20:32 +0800

    Working up to 13

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,17 +1,17 @@
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      false
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+)
 
 (defn factorial [n]
   __)
